c9e78d33b7d424239664bbf2577a13974210b7ba
Andrew <66942330+Noro95@users.noreply.github.com>
2024-08-14 21:24:30 +0200

    Init

---
+++ index.ts
@@ -0,0 +1,66 @@
+import en from "./english.js";
+
+export default {
+	// Indonesian
+	id: null,
+	// Danish
+	da: null,
+	// German
+	de: null,
+	// English, UK
+	"en-GB": en,
+	// English, US
+	"en-US": en,
+	// Spanish
+	"es-ES": null,
+	// French
+	fr: null,
+	// Croatian
+	hr: null,
+	// Italian
+	it: null,
+	// Lithuanian
+	lt: null,
+	// Hungarian
+	hu: null,
+	// Dutch
+	nl: null,
+	// Norwegian
+	no: null,
+	// Polish
+	pl: null,
+	// Portuguese, Brazilian
+	"pt-BR": null,
+	// Romanian, Romania
+	ro: null,
+	// Finnish
+	fi: null,
+	// Swedish
+	sv: null,
+	// Vietnamese
+	vi: null,
+	// Turkish
+	tr: null,
+	// Czech
+	cs: null,
+	// Greek
+	el: null,
+	// Bulgarian
+	bg: null,
+	// Russian
+	ru: null,
+	// Ukrainian
+	uk: null,
+	// Hindi
+	hi: null,
+	// Thai
+	th: null,
+	// Chinese, China
+	"zh-CN": null,
+	// Japanese
+	ja: null,
+	// Chinese, Taiwan
+	"zh-TW": null,
+	// Korean
+	ko: null,
+};
