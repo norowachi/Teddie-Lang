d66b059b5748ff66675ed8fdcab48ef6c227fccc
Noro@CV2 <66942330+Noro95@users.noreply.github.com>
2024-08-16 21:19:10 +0200

    Update language translations

--- index.ts
+++ index.ts
@@ -1,66 +1,68 @@
-import en from "./english.js";
+import en from './english.js';
 
 export default {
-	// Indonesian
-	id: null,
-	// Danish
-	da: null,
-	// German
-	de: null,
-	// English, UK
-	"en-GB": en,
-	// English, US
-	"en-US": en,
-	// Spanish
-	"es-ES": null,
-	// French
-	fr: null,
-	// Croatian
-	hr: null,
-	// Italian
-	it: null,
-	// Lithuanian
-	lt: null,
-	// Hungarian
-	hu: null,
-	// Dutch
-	nl: null,
-	// Norwegian
-	no: null,
-	// Polish
-	pl: null,
-	// Portuguese, Brazilian
-	"pt-BR": null,
-	// Romanian, Romania
-	ro: null,
-	// Finnish
-	fi: null,
-	// Swedish
-	sv: null,
-	// Vietnamese
-	vi: null,
-	// Turkish
-	tr: null,
-	// Czech
-	cs: null,
-	// Greek
-	el: null,
-	// Bulgarian
-	bg: null,
-	// Russian
-	ru: null,
-	// Ukrainian
-	uk: null,
-	// Hindi
-	hi: null,
-	// Thai
-	th: null,
-	// Chinese, China
-	"zh-CN": null,
-	// Japanese
-	ja: null,
-	// Chinese, Taiwan
-	"zh-TW": null,
-	// Korean
-	ko: null,
+    // Indonesian
+    id: { native: 'Bahasa Indonesia', lang: null },
+    // Danish
+    da: { native: 'Dansk', lang: null },
+    // German
+    de: { native: 'Deutsch', lang: null },
+    // English, UK
+    'en-GB': { native: 'English, UK', lang: en },
+    // English, US
+    'en-US': { native: 'English, US', lang: en },
+    // Spanish
+    'es-ES': { native: 'Español', lang: null },
+    // Spanish, LATAM
+    'es-419': { native: 'Español, LATAM', lang: null },
+    // French
+    fr: { native: 'Français', lang: null },
+    // Croatian
+    hr: { native: 'Hrvatski', lang: null },
+    // Italian
+    it: { native: 'Italiano', lang: null },
+    // Lithuanian
+    lt: { native: 'Lietuviškai', lang: null },
+    // Hungarian
+    hu: { native: 'Magyar', lang: null },
+    // Dutch
+    nl: { native: 'Nederlands', lang: null },
+    // Norwegian
+    no: { native: 'Norsk', lang: null },
+    // Polish
+    pl: { native: 'Polski', lang: null },
+    // Portuguese, Brazilian
+    'pt-BR': { native: 'Português do Brasil', lang: null },
+    // Romanian, Romania
+    ro: { native: 'Română', lang: null },
+    // Finnish
+    fi: { native: 'Suomi', lang: null },
+    // Swedish
+    'sv-SE': { native: 'Svenska', lang: null },
+    // Vietnamese
+    vi: { native: 'Tiếng Việt', lang: null },
+    // Turkish
+    tr: { native: 'Türkçe', lang: null },
+    // Czech
+    cs: { native: 'Čeština', lang: null },
+    // Greek
+    el: { native: 'Ελληνικά', lang: null },
+    // Bulgarian
+    bg: { native: 'български', lang: null },
+    // Russian
+    ru: { native: 'Pусский', lang: null },
+    // Ukrainian
+    uk: { native: 'Українська', lang: null },
+    // Hindi
+    hi: { native: 'हिन्दी', lang: null },
+    // Thai
+    th: { native: 'ไทย', lang: null },
+    // Chinese, China
+    'zh-CN': { native: '中文', lang: null },
+    // Japanese
+    ja: { native: '日本語', lang: null },
+    // Chinese, Taiwan
+    'zh-TW': { native: '繁體中文', lang: null },
+    // Korean
+    ko: { native: '한국어', lang: null },
 };
